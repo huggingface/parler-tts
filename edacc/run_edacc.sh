746881243f050cfdfcafea92c69f0f0c3b8bf670
sanchit-gandhi <sanchit@huggingface.co>
2024-02-21 14:07:28 +0000

    use normalised labels

--- edacc/run_edacc.sh
+++ edacc/run_edacc.sh
@@ -3,5 +3,5 @@
 python prepare_edacc.py \
     --dataset_dir "/fsx/sanchit/edacc/edacc_v1.0" \
     --output_dir "/fsx/sanchit/edacc_processed" \
-    --hub_dataset_id "sanchit-gandhi/edacc" \
-    --push_to_hub True
+    --hub_dataset_id "sanchit-gandhi/edacc-normalized" \
+    --push_to_hub
