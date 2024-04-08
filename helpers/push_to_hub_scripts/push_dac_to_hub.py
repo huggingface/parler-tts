6e6b2997063f7c43a9784a8a7b65a740880b82cf
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-04-08 23:58:08 +0200

    add push to hub scripts

--- helpers/push_to_hub_scripts/push_dac_to_hub.py
+++ helpers/push_to_hub_scripts/push_dac_to_hub.py
@@ -1,24 +1,16 @@
 import dac
+from parler_tts import DACConfig, DACModel
+from transformers import AutoConfig, AutoModel
+from transformers import EncodecFeatureExtractor
+AutoConfig.register("dac", DACConfig)
+AutoModel.register(DACConfig, DACModel)
 
 # Download a model
 model_path = dac.utils.download(model_type="44khz")
 model = dac.DAC.load(model_path)
 
-from parler_tts import DACConfig, DACModel
-
 hf_dac = DACModel(DACConfig())
 hf_dac.model.load_state_dict(model.state_dict())
 
-from transformers import AutoConfig, AutoModel
-
-AutoConfig.register("dac", DACConfig)
-AutoModel.register(DACConfig, DACModel)
-
 hf_dac.push_to_hub("ylacombe/dac_44khZ_8kbps")
-
-# DACConfig.register_for_auto_class()
-# DACModel.register_for_auto_class("AutoModel")
-
-from transformers import EncodecFeatureExtractor
-
 EncodecFeatureExtractor(sampling_rate=44100).push_to_hub("ylacombe/dac_44khZ_8kbps")
