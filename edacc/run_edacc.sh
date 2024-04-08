db56636587f1125173fb2a0c255ef2ed53b98339
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-04-08 18:51:12 +0200

    clean artifacts

--- edacc/run_edacc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python prepare_edacc.py \
-    --dataset_dir "/fsx/sanchit/edacc/edacc_v1.0" \
-    --output_dir "/fsx/sanchit/edacc_processed" \
-    --hub_dataset_id "sanchit-gandhi/edacc-normalized" \
-    --push_to_hub
