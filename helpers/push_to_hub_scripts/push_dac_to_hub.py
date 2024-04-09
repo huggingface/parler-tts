fa0d258d365601c4c3174b89352dd27cd80b54bd
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-04-09 13:11:53 +0200

    update license and copyright terms

--- helpers/push_to_hub_scripts/push_dac_to_hub.py
+++ helpers/push_to_hub_scripts/push_dac_to_hub.py
@@ -12,5 +12,5 @@ model = dac.DAC.load(model_path)
 hf_dac = DACModel(DACConfig())
 hf_dac.model.load_state_dict(model.state_dict())
 
-hf_dac.push_to_hub("ylacombe/dac_44khZ_8kbps")
-EncodecFeatureExtractor(sampling_rate=44100).push_to_hub("ylacombe/dac_44khZ_8kbps")
+hf_dac.push_to_hub("parler-tts/dac_44khZ_8kbps")
+EncodecFeatureExtractor(sampling_rate=44100).push_to_hub("parler-tts/dac_44khZ_8kbps")
