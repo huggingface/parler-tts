ef4065428aa045cb5b059884f54da5f5e06bceb1
yoach@huggingface.co <Yoach Lacombe>
2024-06-07 08:01:44 +0000

    remove old benchmarks

--- helpers/benchmark/dataset.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("parler-tts/libritts_r_tags_tagged_10k_generated", 'clean')
-
-PROMPTS = dataset['test.clean']['text']
-DESCRIPTIONS = dataset['test.clean']['text_description']
-
