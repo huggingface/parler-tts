7f1a7627fae56387b5e118487922463712562759
sidhantls <sidhant96@outlook.com>
2024-10-27 15:25:47 +0000

    fix peft by init 0

--- training/peft_utils.py
+++ training/peft_utils.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+from tqdm import tqdm 
 
 class LoRALinear(nn.Module):
     def __init__(self, linear_layer, lora_r, lora_alpha, lora_dropout):
@@ -10,6 +12,10 @@ class LoRALinear(nn.Module):
         
         self.lora_A = nn.Linear(linear_layer.in_features, lora_r, bias=False)
         self.lora_B = nn.Linear(lora_r, linear_layer.out_features, bias=False)
+
+        init.zeros_(self.lora_A.weight)
+        init.zeros_(self.lora_B.weight)
+
         self.scaling = self.lora_alpha / self.lora_r
         
     def forward(self, x):
@@ -17,7 +23,7 @@ class LoRALinear(nn.Module):
         x = self.linear(x) + self.lora_B(self.lora_A(x)) * self.scaling
         return x
 
-def replace_linear_with_lora(model, lora_r, lora_alpha, lora_dropout):
+def replace_linear_with_lora_old(model, lora_r, lora_alpha, lora_dropout):
     for name, module in model.named_modules():
         if any(item in name for item in ['embed_prompts', 'lm_heads']):
             print('Ignored adding peft to ', name)
@@ -26,6 +32,43 @@ def replace_linear_with_lora(model, lora_r, lora_alpha, lora_dropout):
         if isinstance(module, nn.Linear):
             lora_linear = LoRALinear(module, lora_r, lora_alpha, lora_dropout)
             setattr(model, name, lora_linear)
+    return model
+
+def replace_linear_with_lora(model, lora_r, lora_alpha, lora_dropout):
+    full_name_dict = {module: name for name, module in model.named_modules()}
+    linear_info = {}
+    modules = [model]
+    while len(modules) > 0:
+        submodule = modules.pop()
+        for name, raw_linear in submodule.named_children():
+            if isinstance(raw_linear, torch.nn.Linear):
+                full_name = full_name_dict[raw_linear]
+                linear_info[raw_linear] = {
+                    "father": submodule,
+                    "name": name,
+                    "full_name": full_name,
+                }
+            else:
+                modules.append(raw_linear)
+
+    for total_len, _ in enumerate(model.named_modules()):
+        pass
+
+    i = 0
+    for name, module in tqdm(model.named_modules(), total=total_len, desc='Replacing Linear with Low-Rank Layers', mininterval=5):
+        if any(item in name for item in ['embed_prompts', 'lm_heads']):
+        	print('Ignored adding peft to ', name)
+
+        elif module in linear_info:
+            info = linear_info[module]
+            new_module = LoRALinear(module, lora_r, lora_alpha, lora_dropout)
+            setattr(info["father"], info["name"], new_module)
+
+            del linear_info[module]
+            torch.cuda.empty_cache()
+
+    torch.cuda.empty_cache()
+    print('Replaced linear layers with low-rank layers.')
     return model
 
 def set_non_lora_gradients_to_false(model):
